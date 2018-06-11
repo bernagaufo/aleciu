37e20bb56d8bb85c5f96e9c13bd0985c10372d5d
Rafael Robles <gdl.villber@gmail.com>
2018-06-10 22:13:29 -0500

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,43 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+function exitFromApp(){
+    if (navigator.app) {
+        navigator.app.exitApp();
+    } else if (navigator.device) {
+        navigator.device.exitApp();
+    } else {
+        window.close();
+    }
+};
+function onDeviceReady(){
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+function onBackKeyDown(){
+    alert('Atrás');
+    return false;
+};
+app.initialize();
