7cee1789ba47621292b24b2663ab8ebae97efd47
Rafael Robles <gdl.villber@gmail.com>
2018-06-25 00:00:50 -0500

    quite plugin admob

--- www/js/index.js
+++ www/js/index.js
@@ -12,53 +12,6 @@ var app = {
         this.receivedEvent('deviceready');
         document.addEventListener("backbutton", onBackKeyDown, false);
         // Probar los banner admob
-        //initialize the goodies
-        function initAd(){
-                if ( window.plugins && window.plugins.AdMob ) {
-                    var ad_units = {
-                        android : {
-                            banner: 'ca-app-pub-0211823635398974/8309002577',       //PUT ADMOB ADCODE HERE
-                            interstitial: 'ca-app-pub-0211823635398974/7783943294'  //PUT ADMOB ADCODE HERE
-                        }
-                    };
-                    var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
-
-                    window.plugins.AdMob.setOptions( {
-                        publisherId: admobid.banner,
-                        interstitialAdId: admobid.interstitial,
-                        adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,  //use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD
-                        bannerAtTop: false, // set to true, to put banner at top
-                        overlap: true, // banner will overlap webview
-                        offsetTopBar: false, // set to true to avoid ios7 status bar overlap
-                        isTesting: false, // receiving test ad
-                        autoShow: true // auto show interstitial ad when loaded
-                    });
-
-                    registerAdEvents();
-                } else {
-                    //alert( 'admob plugin not ready' );
-                }
-        }
-        //functions to allow you to know when ads are shown, etc.
-        function registerAdEvents() {
-                document.addEventListener('onReceiveAd', function(){});
-                document.addEventListener('onFailedToReceiveAd', function(data){});
-                document.addEventListener('onPresentAd', function(){});
-                document.addEventListener('onDismissAd', function(){ });
-                document.addEventListener('onLeaveToAd', function(){ });
-                document.addEventListener('onReceiveInterstitialAd', function(){ });
-                document.addEventListener('onPresentInterstitialAd', function(){ });
-                document.addEventListener('onDismissInterstitialAd', function(){ });
-            }
-        //display the banner
-        function showBannerFunc(){
-            window.plugins.AdMob.createBannerView();
-        }
-        //display the interstitial
-        function showInterstitialFunc(){
-            window.plugins.AdMob.createInterstitialView();  //get the interstitials ready to be shown and show when it's loaded.
-            window.plugins.AdMob.requestInterstitialAd();
-        }
     },
 
     // Update DOM on a Received Event
