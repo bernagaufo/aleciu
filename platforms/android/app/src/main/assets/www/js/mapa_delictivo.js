c049a4055acc6cd6fb009c35dabc903964d9afb4
Rafael Robles <gdl.villber@gmail.com>
2018-06-24 23:03:38 -0500

    puse splash, correcion de datos

---
+++ platforms/android/app/src/main/assets/www/js/mapa_delictivo.js
@@ -0,0 +1,84 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBdc0ImML1OYhvoFtOdhU4m8TTbCCBGtzI",
+    authDomain: "alertasc-cc11f.firebaseapp.com",
+    databaseURL: "https://alertasc-cc11f.firebaseio.com",
+    projectId: "alertasc-cc11f",
+    storageBucket: "alertasc-cc11f.appspot.com",
+    messagingSenderId: "949793015710"
+  };
+  firebase.initializeApp(config);
+
+
+      var citymap = {
+        chicago: {
+          center: {lat: 41.878, lng: -87.629},
+          population: 2714856
+        },
+        newyork: {
+          center: {lat: 40.714, lng: -74.005},
+          population: 8405837
+        },
+        losangeles: {
+          center: {lat: 34.052, lng: -118.243},
+          population: 3857799
+        },
+        vancouver: {
+          center: {lat: 49.25, lng: -123.1},
+          population: 603502
+        }
+      };
+
+      function initMap() {
+        // Create the map.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: {lat: 37.090, lng: -95.712},
+          mapTypeId: 'terrain'
+        });
+
+        // Construct the circle for each value in citymap.
+        // Note: We scale the area of the circle based on the population.
+        for (var city in citymap) {
+          // Add the circle for this city to the map.
+          var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: citymap[city].center,
+            radius: Math.sqrt(citymap[city].population) * 100
+          });
+        }
+      };
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+
+    };
+    var marker = new google.maps.Marker({
+      icon: iconBase + 'mipos.png',
+      position: pos,
+      map: map,
+      title: 'MiPosicion'
+    });
+    // infoWindow.setPosition(pos);
+    // infoWindow.setContent('Estas aqu&iacute;.');
+    map.setCenter(pos);
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
+  });
+} else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+};    
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+};
